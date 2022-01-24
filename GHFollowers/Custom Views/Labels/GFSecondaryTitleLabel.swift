8cbc9595a4d4b41d04b495e1390871a373a0ca73
haji44 <61269325+haji44@users.noreply.github.com>
2022-01-24 14:13:53 +0900

    Child View Controller
    Child ViewControllers
    ViewControllers vs. UIViews
    SecondaryTitleLabel
    SFSymbols
    Constants File

---
+++ GHFollowers/Custom Views/Labels/GFSecondaryTitleLabel.swift
@@ -0,0 +1,39 @@
+//
+//  GFSecondaryTitleLabel.swift
+//  GHFollowers
+//
+//  Created by kitano hajime on 2022/01/24.
+//  Copyright © 2022 Sean Allen. All rights reserved.
+//
+
+import UIKit
+
+// This class is used in userinfoVC
+class GFSecondaryTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    // because this label always assing left side,
+    // in that case we should set font size instead of text allinment
+    init(fontSize: CGFloat) {
+        super.init(frame: .zero)
+        font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.90
+        lineBreakMode = .byTruncatingTail // generally title should be one line
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
---
+++ GHFollowers/Utitlties/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by kitano hajime on 2022/01/24.
+//  Copyright © 2022 Sean Allen. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbol {
+    static let location = "mappin.and.ellipse"
+}
